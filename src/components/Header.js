68a783622231b85059d9f0753f1fb7bc22ceadef
Jose Torres <jtorresocc@gmail.com>
2022-12-29 22:49:10 -0800

    Added ProductScreen page.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Nav, Container, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
   return (
@@ -7,12 +8,22 @@ function Header() {
       {" "}
       <Navbar bg="dark" expand="lg" variant="dark" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">React Shop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>React Shop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link href="/cart"><i className="fas fa-shopping-cart "></i> Cart</Nav.Link>
-              <Nav.Link href="/login"><i className="fas fa-user"></i> Login</Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart "></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i> Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
